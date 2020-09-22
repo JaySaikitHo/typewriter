e3acf0dee42757a6ec9233b1d550bbd3ce472f24
vagrant <ttjho@hotmail.com>
2020-09-22 20:43:33 +0000

    used setTimeout to make typerwrter effect

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+let timer = 150
+const typewriter = function(sentence){
+  console.log()
+  
+  for(const word of sentence) {
+    timer +=150
+    setTimeout(()=>{process.stdout.write(word)},timer)
+    
+  
+  }
+  
+}
+
+typewriter(sentence)
 
